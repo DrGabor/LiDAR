bacab6330e9618664271353e226af58a32ba4409
Di Wang <35476470+DrGabor@users.noreply.github.com>
2018-04-17 02:46:58 +0000

    Delete main.m

--- GPSegmentation/main.m
+++
@@ -1,7 +0,0 @@
-clc; close all; clear all;
-%%%%%%%%% this directory should be properly changed. 
-addpath('GPSegmentation\Ransac\');  
-load data.mat
-GapThr = [0.1 5.0]; 
-IS_SHOW = 1; 
-[EffIdx, NffIdx, UnkownIdx] = GPSegFun(data, GapThr, IS_SHOW); 
