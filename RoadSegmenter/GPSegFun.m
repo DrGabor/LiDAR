c95c799f41c971aa7d5cae788c42a59a0ac75358
Di Wang <35476470+DrGabor@users.noreply.github.com>
2018-07-11 14:47:11 +0800

    Update GPSegFun.m

--- RoadSegmenter/GPSegFun.m
+++ RoadSegmenter/GPSegFun.m
@@ -1,9 +1,18 @@
-function [GrdIdx, ObsIdx, UnkownIdx] = GPSegFun(pcData, GapThr, IS_SHOW)
-if size(pcData, 1) ~= 3 
-    error('pcData must be 3xN matrix!'); 
+function [GrdIdx, ObsIdx, UnkownIdx, GrdPts] = GroundSegmentationFunWithFixedHyp(pcData, GapThr, IS_SHOW)
+if nargin == 0
+    clc; close all;
+    DataFolder = 'D:\Data\Sudoku\Record-2017-10-26-09-03-48(Match2016)\BinaryData';
+    nFrm = 655;
+    DataDir = fullfile(DataFolder, sprintf('Binary%d.txt', nFrm));
+    pcData = HDLAnalyserNew(DataDir);
+    pcData = pcData(1:3, :);
+    %     idx = find(pcData(1, :) >= 0);
+    %     pcData = pcData(:, idx);
+    GapThr = [0.10 Inf];
+    IS_SHOW = 1;
 end
-%% partition as polar coordinate. 
-RadArray = 0.0 : 1.0 : 80.0;
+RadRes = 1.0; 1.0;
+RadArray = 0.0 : RadRes : 80.0;
 AngRes = deg2rad(5.0);
 [SegID, BinID, EffIdx] = CvtPtsToPolar(pcData, RadArray, AngRes);
 SegID = SegID(EffIdx);
@@ -44,7 +53,7 @@ ss = struct('data', [] );
 RMInfo = repmat(ss, maxAngLen, maxBinLen);
 verbose = 0;
 UnkownIdx = [];
-%% start GP regression. 
+rand('state',0);
 for AngID = 1 : 1 : maxAngLen
     SelIdx = find(CoorI(1, :) == AngID );
     SelPts = [];
@@ -64,17 +73,18 @@ for AngID = 1 : 1 : maxAngLen
         end
         continue;
     end
-    %%%%%% ransac to find a line. 
+    %%%%%%%
     Dist = sqrt( SelPts(1, :).^2 + SelPts(2, :).^2 );
     Pts = [Dist; SelPts(3, :)];
+    DistThr = 0.2;
     [V, L, inliers] = ransacfit2Dline(Pts, 0.1, 0);
     Dist = ( Pts(1, :)*V(1) + Pts(2, :)*V(2)+V(3) ) / norm(V(1:2));
     %%%%% y = kx + b.
     k = -V(1)/V(2);
     b = -V(3)/V(2);
-
-    H0 = k*5.0+b;
-    idx = find(abs(Dist) <= 0.10);
+    testRad = 5.0;
+    H0 = k*testRad + b;
+    idx = find(abs(Dist) <= DistThr);
     if length(idx) < 10 | ( H0 > -1.5 | H0 < -2.1)  % the orignal ground height is -1.8m.
         UnkownSeg0 = [UnkownSeg0 AngID];
         if length(idx) < 10
@@ -93,19 +103,24 @@ for AngID = 1 : 1 : maxAngLen
     end
     tData = 2.0;
     tDist = 0.15;
+    % save('./GInfo.mat');
+    % out = GPRGrdSegFun(Pts, idx', tData, tDist, [], 0);
     Hyp.cov = log([6.0 sqrt(1.3298)]);
     Hyp.mean = [0 0];
     Hyp.lik = log(0.1);
-    out = GPR_SelectFun(Pts, idx, tData, tDist, Hyp); 
+    out = GPRGrdSegFun_WithFixedHyp(Pts, idx, tData, tDist, Hyp);
     VIdx = out.ValidIdx;
     xs = Pts(1, :);
     ys = Pts(2, :);
-    data_t = out.data(out.ValidIdx, :); 
+    data_t = out.data(out.ValidIdx, :);
     [mu, s2, F, EffIdx, NffIdx] = GPR_PredictFun(out.hyp, data_t, xs, ys, tData, tDist);
-    
+    [H0, ~] = myGPRFun(out.hyp, data_t(:, 1), data_t(:, 2), testRad);
+    if ( H0 > -1.5 | H0 < -2.1 )
+        UnkownSeg0 = [UnkownSeg0 AngID];
+        continue;
+    end
     CorrectH = ys';
     CorrectH(NffIdx) = mu(NffIdx);
-    % CorrectH = mu;
     for i = 1 : 1 : length(SelIdx)
         tmp = TotalPixel(SelIdx(i));
         Pts = pcData(:, tmp.RawIdx);
@@ -123,25 +138,9 @@ for AngID = 1 : 1 : maxAngLen
         MPt(3) = CorrectH(i);
         RMInfo( tmp.CoorI(1), tmp.CoorI(2) ).data = MPt;
     end
+    bTest = 1;
 end
-if IS_SHOW
-    figure;
-    hold on;
-    grid on;
-    axis equal;
-    % view(3);
-    for i = 1 : 1 : size(RMInfo, 1)
-        x = i*ones(1, size(RMInfo, 2));
-        y = 1:1:size(RMInfo, 2);
-        ind = sub2ind(size(RMInfo), x, y);
-        tmpPts = cat(2, RMInfo(ind).data);
-        if ~isempty(tmpPts)
-            plot3(tmpPts(1, :), tmpPts(2, :), tmpPts(3, :), 'b.-');
-        end
-    end
-    title('Road Model');
-end
-%% check the unkown segments.  
+%% check the unkown segments.
 GrdPts = cat(2, RMInfo(:).data);
 GrdMd = createns(GrdPts');
 UnkownSeg = [];
@@ -176,7 +175,8 @@ for id = 1:1:length(UnkownSeg0)
         tmpPts = GrdPts(:, NNIdx);
         H = mean(tmpPts(3, :));
         tmpDiff = Pts(3, :) - H;
-        idx = find(tmpDiff >= 0.3 & tmpDiff <= GapThr(2) );
+        % tmpDiff = abs(tmpDiff);
+        idx = find(tmpDiff >= GapThr(1) & tmpDiff <= GapThr(2) );
         NewObsIdx = [NewObsIdx tmp.RawIdx(idx)];
         tmpIdx = find(tmpDiff > GapThr(2));
         UnkownIdx = [UnkownIdx tmp.RawIdx(tmpIdx)];
@@ -195,15 +195,53 @@ for id = 1:1:length(UnkownSeg0)
     end
     bTest = 1;
 end
-ObsIdx = [ObsIdx NewObsIdx];    
+ObsIdx = [ObsIdx NewObsIdx];
 Dist = sqrt(pcData(1, :).^2 + pcData(2, :).^2);
-    NIdx = find(Dist > RadArray(end) );
-    GrdIdx = find(~ismember(1:1:size(pcData,2), ObsIdx) & ~ismember(1:1:size(pcData,2), UnkownIdx) & ~ismember(1:1:size(pcData,2), NIdx) );
+NIdx = find(Dist > RadArray(end) );
+GrdIdx = find(~ismember(1:1:size(pcData,2), ObsIdx) & ~ismember(1:1:size(pcData,2), UnkownIdx) & ~ismember(1:1:size(pcData,2), NIdx) );
+GrdMd = createns(GrdPts(1:2, :)');
+GrdAddPts = []; 
+for i = 1 : 1 : size(RMInfo, 1)
+    x = i*ones(1, size(RMInfo, 2));
+    y = 1:1:size(RMInfo, 2);
+    ind = sub2ind(size(RMInfo), x, y);
+    Tmp = cat(2, RMInfo(ind).data);
+    if isempty(Tmp)
+        SelIdx = find(CoorI(1, :) == i );
+        Tmp = [];
+        for i = 1 : 1 : length(SelIdx)
+            tmpInfo = TotalPixel(SelIdx(i));
+            coorI = tmpInfo.CoorI;
+            angId = coorI(1);
+            radId = coorI(2);
+            ang = (angId - 0.5)*AngRes;
+            r = RadArray(radId) + 0.5*RadRes;
+            pt = [r*cos(ang); r*sin(ang)];
+            
+            for rId = 1:1:length(rArray)
+                [NNIdx, DD] = rangesearch(GrdMd, pt', rArray(rId));
+                if ~isempty(NNIdx{1})
+                    break;
+                end
+            end
+            NNIdx = NNIdx{1};
+            tmpPts = GrdPts(:, NNIdx);
+            h = mean(tmpPts(3, :));
+            pt(end+1) = h;
+            %                 Pts = pcData(:, tmpInfo.RawIdx);
+            %                 [~, id] = min(Pts(3, :));
+            GrdAddPts(:, end+1) = pt;
+            bTest = 1;
+        end
+    end
+end
+GrdPts = [GrdPts GrdAddPts];
 if IS_SHOW
     figure;
     hold on;
     grid on;
     view(3);
+    
     if ~isempty(GrdIdx)
         showPointCloud(pcData(1:3, GrdIdx)', 'g');
     end
@@ -213,7 +251,21 @@ if IS_SHOW
     if ~isempty(ObsIdx)
         showPointCloud(pcData(1:3, ObsIdx)', 'r');
     end
-    title('Segmentation Results');
+    title('Segmentation via GPR');
+    xlabel('X/m');
+    ylabel('Y/m');
+    zlabel('Z/m');
+    figure; 
+    hold on; 
+    grid on; 
+    axis equal; 
+    pcshow(pcData(1:3, :)'); 
+    plot3(GrdPts(1, :), GrdPts(2, :), GrdPts(3, :), 'ro', 'markersize', 3, 'markerfacecolor', 'r', ...
+        'markeredgecolor', 'y'); 
+    xlabel('X/m');
+    ylabel('Y/m');
+    zlabel('Z/m');
+    title('Road Model'); 
 end
 end
 
